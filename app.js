c7fdf0cc3aec8c17269524a754a6acdf21362078
Mark Centoni <marklc44@gmail.com>
2014-07-22 10:07:19 -0700

    added http request module

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
-var express = require("express");
+var express = require("express"),
+		ejs = require('ejs'),
+		expressLayouts = require('express-ejs-layouts');
 
 var app = express();
 
+// set static assets folder also
+app.set('layout', 'layout');
+app.set('view engine', 'ejs');
+
+app.use(expressLayouts);
+
+// Data
+var query;
+var ombdUrl = 'http://www.omdbapi.com/?';
+
 app.get('/', function(req, res){
-  res.render('index.ejs');
+  res.render('index');
 });
+
+app.get('/search', function(req, res) {
+	query = req.query.searchTerm;
+	res.send('search page ' + query);
+})
+
 
 app.listen(3000);
