ea9e681d61872fb9c168c2cdca1a2df60a1443e5
Mark Centoni <marklc44@gmail.com>
2014-07-22 11:32:44 -0700

    movie search results displayed in results template

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express"),
 		ejs = require('ejs'),
-		expressLayouts = require('express-ejs-layouts');
+		expressLayouts = require('express-ejs-layouts'),
+		request = require('request');
 
 var app = express();
 
@@ -9,10 +10,11 @@ app.set('layout', 'layout');
 app.set('view engine', 'ejs');
 
 app.use(expressLayouts);
+app.use('/assets', express.static(__dirname + '/assets'));
 
 // Data
 var query;
-var ombdUrl = 'http://www.omdbapi.com/?';
+var omdbUrl = 'http://www.omdbapi.com/?s=';
 
 app.get('/', function(req, res){
   res.render('index');
@@ -20,8 +22,27 @@ app.get('/', function(req, res){
 
 app.get('/search', function(req, res) {
 	query = req.query.searchTerm;
-	res.send('search page ' + query);
-})
+	// res.send('search page ' + query);
+
+	request(omdbUrl + query, function(error, response, body) {
+		if(!error && response.statusCode === 200) {
+			var body = JSON.parse(body);
+			if (body.Search) {
+				res.render('results', {
+				movies: body.Search,
+				query: query
+			});
+			} else {
+				res.render('results', {
+					movies: [
+						{Title: 'No movies Found', Year: ''}
+					]
+				});
+			}
+		}
+	});
+
+});
 
 
 app.listen(3000);
