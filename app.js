930726589d627095af25cab3c2a348bb6690b779
Mark Centoni <marklc44@gmail.com>
2014-07-22 17:37:41 -0700

    saving to favorites working

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 var express = require("express"),
 		ejs = require('ejs'),
 		expressLayouts = require('express-ejs-layouts'),
+		bodyParser = require('body-parser'),
 		request = require('request');
 
 var app = express();
 
-// set static assets folder also
+// View Engine, Layout, Assets
 app.set('layout', 'layout');
 app.set('view engine', 'ejs');
-
 app.use(expressLayouts);
-app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/public', express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded());
 
 // Data
 var query;
-var omdbUrl = 'http://www.omdbapi.com/?s=';
+var omdbUrl = 'http://www.omdbapi.com/?';
+var favorites = [];
 
 app.get('/', function(req, res){
   res.render('index');
@@ -22,9 +24,8 @@ app.get('/', function(req, res){
 
 app.get('/search', function(req, res) {
 	query = req.query.searchTerm;
-	// res.send('search page ' + query);
 
-	request(omdbUrl + query, function(error, response, body) {
+	request(omdbUrl + 's=' + query, function(error, response, body) {
 		if(!error && response.statusCode === 200) {
 			var body = JSON.parse(body);
 			if (body.Search) {
@@ -35,13 +36,48 @@ app.get('/search', function(req, res) {
 			} else {
 				res.render('results', {
 					movies: [
-						{Title: 'No movies Found', Year: ''}
-					]
+						{ Title: 'No movies Found', Year: '' }
+					], 
+					query: query
 				});
 			}
 		}
 	});
 
+});
+
+// get with request to title
+app.get('/movies/:id', function(req, res) {
+	query = req.params.id;
+	console.log(query);
+
+	request(omdbUrl + 'i=' + query, function(error, response, body) {
+		if(!error && response.statusCode === 200) {
+			var body = JSON.parse(body);
+			if (body) {
+				res.render('show', {
+				movie: body
+			});
+			} else {
+				res.render('show', {
+					movie: [
+						{ Title: 'No movies Found', Year: '' }
+					], 
+					query: query
+				});
+			}
+		}
+	});
+
+});
+
+app.post('/favorites', function(req, res) {
+	favorites.push(req.body.movie);
+	res.redirect('/favorites');
+});
+
+app.get('/favorites', function(req, res) {
+	res.render('favorites', { faves: favorites });
 });
 
 
